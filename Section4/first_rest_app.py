a9a5af1719618c35fa9a4c7f01cb23bf028e5d69
Tanmay Bhatnagar <tanmaybhatnagar84976@gmail.com>
2018-11-09 23:39:17 +0530

    Added login auth

--- Section4/first_rest_app.py
+++ Section4/first_rest_app.py
@@ -1,11 +1,18 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
+
 
 app = Flask(__name__)
 api = Api(app)
-
+app.secret_key = 'tanmay'
+ 
+jwt = JWT(app , authenticate , identity) #/auth (automatically generated
+                                                 #   end point by JWT)
 items = []
 class Item(Resource):
+    @jwt_required()
     def get(self,name):
         for item in items:
             if item['name'] == name:
